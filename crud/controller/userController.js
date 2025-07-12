f0f29d7750c03924c2140919ecb068812d5a8996
vipulkumar82 <vipulkumar212003@gmail.com>
2025-07-12 13:13:37 +0530

    Update user performed

--- crud/controller/userController.js
+++ crud/controller/userController.js
@@ -11,11 +11,11 @@ export async function createUser(req, res) {
     }
     const { name, password, email } = body;
 
-    const user = await new userModel({
+    const user = await userModel.create({
       name,
       password,
       email,
-    }).save();
+    })
 
     return res.status(200).json({
       message: "New User Created Succefully",
@@ -33,12 +33,42 @@ export const getAllUser = async (req, res) => {
   try {
     const allUser = await userModel.find({});
     return res.status(200).json({
-      users:allUser,
+      users: allUser,
       message: "Got All user",
     });
   } catch (error) {
     return res.status(500).json({
       message: `Error${error.message}`,
+    });
+  }
+};
+
+//CRUD-> U
+// {
+//   "id":"jdksfksdjfklsjfklsjf"
+// }
+
+export const updateUser = async (req, res) => {
+  if (!req.body || !req.body.id || !req.body.email) {
+    return res.status(400).json({
+      success: false,
+      message: "all fields are required to update",
+    }); 
+  }
+  const { id, email } = req.body;
+  try {
+    const updatedUser = await userModel
+      .findByIdAndUpdate(id, {
+        email,
+      })
+
+    return res.status(200).json({
+      message: "User Updated Successfully...",
+      updatedUser: updatedUser,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: `Error:-> ${error.message}`,
     });
   }
 };
--- crud/routes/routes.js
+++ crud/routes/routes.js
@@ -2,9 +2,10 @@
 // const router = express.Router();
 
 import { Router } from "express";
-import { createUser, getAllUser } from "../controller/userController.js";
+import { createUser, getAllUser, updateUser } from "../controller/userController.js";
 
 const router =Router();
 router.post('/create-user',createUser);
 router.get('/', getAllUser);
+router.put('/update-user',updateUser)
 export default router;
